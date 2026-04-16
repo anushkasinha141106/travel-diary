19fd9ae43c49322c37e62db4fd23701bb822750d
anushkasinha141106 <112415024@cse.iiitp.ac.in>
2026-04-17 00:07:35 +0530

    fixed cors origin frontend url

--- backend/index.js
+++ backend/index.js
@@ -50,7 +50,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://travel-diary-frontend-gamma.vercel.app/"
+      "https://travel-diary-frontend-gamma.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
